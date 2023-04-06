bcb06ad510a112731d8ec04cd7791b97e10a7d0d
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-04-06 09:58:30 +0200

    Update MMM-ZAPTEC.js

--- MMM-ZAPTEC.js
+++ MMM-ZAPTEC.js
@@ -1,16 +1,59 @@
 Module.register("MMM-ZAPTEC", {
-  // Define module defaults
-  defaults: {},
+  // Default module config.
+  defaults: {
+    bearerToken: "",
+    updateInterval: 60000 // update every minute
+  },
 
-  // Override start method.
+  // Define start sequence.
   start: function() {
-    this.message = "Hello World";
+    Log.info("Starting module: " + this.name);
+    this.chargerData = [];
+    this.getData();
+    this.scheduleUpdate();
   },
 
   // Override dom generator.
   getDom: function() {
     var wrapper = document.createElement("div");
-    wrapper.innerHTML = this.message;
+    wrapper.className = "small";
+    for (var i = 0; i < this.chargerData.length; i++) {
+      var charger = this.chargerData[i];
+      var chargerWrapper = document.createElement("div");
+      chargerWrapper.innerHTML = "Charger " + (i+1) + " - OperatingMode: " + charger.OperatingMode;
+      wrapper.appendChild(chargerWrapper);
+    }
     return wrapper;
-  }
+  },
+
+  // Fetch charger data.
+  getData: function() {
+    Log.info("Fetching data for module: " + this.name);
+    var self = this;
+    var url = "https://api.zaptec.com/api/chargers/";
+    var token = this.config.bearerToken;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Authorization", "Bearer " + token);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText);
+        self.chargerData = data.Data;
+        self.updateDom();
+      }
+    };
+    xhr.send();
+  },
+
+  // Schedule module update.
+  scheduleUpdate: function(delay) {
+    var self = this;
+    var nextLoad = this.config.updateInterval;
+    if (typeof delay !== "undefined" && delay >= 0) {
+      nextLoad = delay;
+    }
+    setTimeout(function() {
+      self.getData();
+    }, nextLoad);
+  },
 });
