69f29bbebc675af9a5895eb2c39ddff59fc3b2e8
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-04-19 08:41:24 +0200

    Update MMM-ZAPTEC.js

--- MMM-ZAPTEC.js
+++ MMM-ZAPTEC.js
@@ -40,7 +40,7 @@ getDom: function() {
         operatingMode = charger.OperatingMode;
         break;
     }
-    chargerWrapper.innerHTML = "Laddare" + (i+1) + "     " + operatingMode;
+    chargerWrapper.innerHTML = "Laddare " + (i+1) + "     " + operatingMode; // use "Charger" and 5 spaces here
     wrapper.appendChild(chargerWrapper);
   }
   return wrapper;
