5ded42e568e9bcb34471a26725fb055dd37befbe
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-04-20 13:52:00 +0200

    Update MMM-ZAPTEC.js

--- MMM-ZAPTEC.js
+++ MMM-ZAPTEC.js
@@ -5,8 +5,6 @@ Module.register("MMM-ZAPTEC", {
     updateInterval: 60000,
     lang: "swe",
     enableChargeHistory: false,
-    fromDate: "",
-    toDate: "",
     Charger: "all"
   },
 
@@ -14,79 +12,57 @@ Module.register("MMM-ZAPTEC", {
   start: function() {
     Log.info("Starting module: " + this.name);
     this.chargerData = [];
-    this.chargeHistory = [];
     this.sendSocketNotification("GET_CHARGER_DATA", this.config);
     this.scheduleUpdate();
   },
 
 
   // Override dom generator.
-getDom: function() {
-  var wrapper = document.createElement("div");
-  wrapper.className = "small align-left";
+  getDom: function() {
+    var wrapper = document.createElement("div");
+    wrapper.className = "small align-left";
 
-  var chargerIndex = this.config.Charger === "all" ? null : parseInt(this.config.Charger) - 1;
+    var chargerIndex = this.config.Charger === "all" ? null : parseInt(this.config.Charger) - 1;
 
-  for (var i = 0; i < this.chargerData.length; i++) {
-    if (chargerIndex !== null && chargerIndex !== i) {
-      continue;
+    for (var i = 0; i < this.chargerData.length; i++) {
+      if (chargerIndex !== null && chargerIndex !== i) {
+        continue;
+      }
+
+      var charger = this.chargerData[i];
+      var chargerWrapper = document.createElement("div");
+      chargerWrapper.className = "chargerWrapper";
+
+      var lang = this.config.lang;
+      var operatingMode = "";
+      switch (charger.OperatingMode) {
+        case 1:
+          operatingMode = lang === "eng" ? "Available" : "Ledigt";
+          break;
+        case 2:
+          operatingMode = lang === "eng" ? "Authorizing" : "Auktoriserar";
+          break;
+        case 3:
+          operatingMode = lang === "eng" ? "Charging" : "Laddar";
+          break;
+        case 5:
+          operatingMode = lang === "eng" ? "Finished charging" : "Slutade ladda";
+          break;
+        default:
+          operatingMode = charger.OperatingMode;
+          break;
+      }
+
+      chargerWrapper.innerHTML = "Charger " + (i+1) + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + operatingMode;
+      wrapper.appendChild(chargerWrapper);
+
+      if (chargerIndex !== null) {
+        break;
+      }
     }
 
-    var charger = this.chargerData[i];
-    var chargerWrapper = document.createElement("div");
-    chargerWrapper.className = "chargerWrapper";
-
-    var lang = this.config.lang;
-    var operatingMode = "";
-    switch (charger.OperatingMode) {
-      case 1:
-        operatingMode = lang === "eng" ? "Available" : "Ledigt";
-        break;
-      case 2:
-        operatingMode = lang === "eng" ? "Authorizing" : "Auktoriserar";
-        break;
-      case 3:
-        operatingMode = lang === "eng" ? "Charging" : "Laddar";
-        break;
-      case 5:
-        operatingMode = lang === "eng" ? "Finished charging" : "Slutade ladda";
-        break;
-      default:
-        operatingMode = charger.OperatingMode;
-        break;
-    }
-
-    chargerWrapper.innerHTML = "Charger " + (i+1) + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + operatingMode;
-    wrapper.appendChild(chargerWrapper);
-
-    if (chargerIndex !== null) {
-      break;
-    }
-  }
-
-  if (this.config.enableChargeHistory) {
-    var historyWrapper = document.createElement("div");
-    historyWrapper.className = "historyWrapper";
-
-    var historyTable = document.createElement("table");
-    historyTable.className = "small";
-    var headerRow = document.createElement("tr");
-    headerRow.innerHTML = "<th>Energy (kWh)</th>";
-    historyTable.appendChild(headerRow);
-
-    for (var i = 0; i < this.chargeHistory.length; i++) {
-      var history = this.chargeHistory[i];
-      var historyRow = document.createElement("tr");
-      historyRow.innerHTML = "<td>" + history.Energy + "</td>";
-      historyTable.appendChild(historyRow);
-    }
-
-    historyWrapper.appendChild(historyTable);
-    wrapper.appendChild(historyWrapper);
-  }
-
-  return wrapper;
-},
+    return wrapper;
+  },
 
 
   // Schedule module update.
@@ -111,13 +87,6 @@ getDom: function() {
       Log.info("Received charger data");
       this.chargerData = payload.chargerData;
       this.updateDom(1000);
-    } else if (notification === "CHARGE_HISTORY_RESULT") {
-      if (payload.error) {
-        Log.error(`Error getting charge history: ${payload.error}`);
-        return;
-      }
-      Log.info("Received charge history");
-      // TODO: handle charge history data
     }
   }
 });
