b52012c661da7394fb5cc1357a01bbe4feb2a41c
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-04-20 09:42:37 +0200

    Update MMM-ZAPTEC.js

--- MMM-ZAPTEC.js
+++ MMM-ZAPTEC.js
@@ -15,41 +15,54 @@ Module.register("MMM-ZAPTEC", {
   },
 
   // Override dom generator.
-  getDom: function() {
-    var wrapper = document.createElement("div");
-    wrapper.className = "small align-left"; // add align-left class here
+getDom: function() {
+  var wrapper = document.createElement("div");
+  wrapper.className = "small align-left"; // add align-left class here
 
-    for (var i = 0; i < this.chargerData.length; i++) {
-      var charger = this.chargerData[i];
-      var chargerWrapper = document.createElement("div");
-      chargerWrapper.className = "chargerWrapper";
-
-      // Retrieve the appropriate translation based on the language setting
-      var lang = this.config.lang;
-      var operatingMode = "";
-      switch (charger.OperatingMode) {
-        case 1:
-          operatingMode = lang === "eng" ? "Available" : "Ledigt";
-          break;
-        case 2:
-          operatingMode = lang === "eng" ? "Authorizing" : "Auktoriserar";
-          break;
-        case 3:
-          operatingMode = lang === "eng" ? "Charging" : "Laddar";
-          break;
-        case 5:
-          operatingMode = lang === "eng" ? "Finished charging" : "Slutade ladda";
-          break;
-        default:
-          operatingMode = charger.OperatingMode;
-          break;
-      }
-
-      chargerWrapper.innerHTML = "Charger " + (i+1) + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + operatingMode;
-      wrapper.appendChild(chargerWrapper);
+  var chargersToShow = [];
+  if (this.config.Charger === "all") {
+    chargersToShow = this.chargerData;
+  } else if (this.config.Charger === "none") {
+    chargersToShow = [];
+  } else {
+    var chargerIndex = parseInt(this.config.Charger) - 1;
+    if (this.chargerData[chargerIndex]) {
+      chargersToShow.push(this.chargerData[chargerIndex]);
     }
-    return wrapper;
-  },
+  }
+
+  for (var i = 0; i < chargersToShow.length; i++) {
+    var charger = chargersToShow[i];
+    var chargerWrapper = document.createElement("div");
+    chargerWrapper.className = "chargerWrapper";
+
+    // Retrieve the appropriate translation based on the language setting
+    var lang = this.config.lang;
+    var operatingMode = "";
+    switch (charger.OperatingMode) {
+      case 1:
+        operatingMode = lang === "eng" ? "Available" : "Ledigt";
+        break;
+      case 2:
+        operatingMode = lang === "eng" ? "Authorizing" : "Auktoriserar";
+        break;
+      case 3:
+        operatingMode = lang === "eng" ? "Charging" : "Laddar";
+        break;
+      case 5:
+        operatingMode = lang === "eng" ? "Finished charging" : "Slutade ladda";
+        break;
+      default:
+        operatingMode = charger.OperatingMode;
+        break;
+    }
+
+    chargerWrapper.innerHTML = "Charger " + (i+1) + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + operatingMode;
+    wrapper.appendChild(chargerWrapper);
+  }
+
+  return wrapper;
+},
 
   // Schedule module update.
   scheduleUpdate: function(delay) {
