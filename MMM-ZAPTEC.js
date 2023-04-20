65042e9be0532eda16f82a3bad0b5694237f7fae
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-04-20 13:58:35 +0200

    Update MMM-ZAPTEC.js

--- MMM-ZAPTEC.js
+++ MMM-ZAPTEC.js
@@ -1,92 +1,67 @@
 Module.register("MMM-ZAPTEC", {
   // Default module config.
   defaults: {
-    bearerToken: "",
-    updateInterval: 60000,
-    lang: "swe",
-    enableChargeHistory: false,
-    Charger: "all"
+    updateInterval: 60 * 1000, // 1 minute
+    token: "",
+    unit: "kWh"
   },
 
   // Define start sequence.
-  start: function() {
-    Log.info("Starting module: " + this.name);
-    this.chargerData = [];
-    this.sendSocketNotification("GET_CHARGER_DATA", this.config);
+  start: function () {
+    Log.info(`Starting module: ${this.name}`);
+    this.energyData = [];
+    this.loaded = false;
+    this.getData();
     this.scheduleUpdate();
   },
 
-
   // Override dom generator.
-  getDom: function() {
+  getDom: function () {
     var wrapper = document.createElement("div");
-    wrapper.className = "small align-left";
 
-    var chargerIndex = this.config.Charger === "all" ? null : parseInt(this.config.Charger) - 1;
-
-    for (var i = 0; i < this.chargerData.length; i++) {
-      if (chargerIndex !== null && chargerIndex !== i) {
-        continue;
-      }
-
-      var charger = this.chargerData[i];
-      var chargerWrapper = document.createElement("div");
-      chargerWrapper.className = "chargerWrapper";
-
-      var lang = this.config.lang;
-      var operatingMode = "";
-      switch (charger.OperatingMode) {
-        case 1:
-          operatingMode = lang === "eng" ? "Available" : "Ledigt";
-          break;
-        case 2:
-          operatingMode = lang === "eng" ? "Authorizing" : "Auktoriserar";
-          break;
-        case 3:
-          operatingMode = lang === "eng" ? "Charging" : "Laddar";
-          break;
-        case 5:
-          operatingMode = lang === "eng" ? "Finished charging" : "Slutade ladda";
-          break;
-        default:
-          operatingMode = charger.OperatingMode;
-          break;
-      }
-
-      chargerWrapper.innerHTML = "Charger " + (i+1) + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + operatingMode;
-      wrapper.appendChild(chargerWrapper);
-
-      if (chargerIndex !== null) {
-        break;
-      }
+    if (!this.loaded) {
+      wrapper.innerHTML = this.translate("LOADING");
+      wrapper.className = "dimmed light small";
+      return wrapper;
     }
 
+    var energy = this.energyData[0];
+    var energyValue = energy ? energy.toFixed(2) : "-";
+    var energyUnit = this.config.unit;
+    var label = this.translate("ENERGY_LABEL");
+
+    wrapper.innerHTML = `${label}: ${energyValue} ${energyUnit}`;
+    wrapper.className = "small";
     return wrapper;
   },
 
-
-  // Schedule module update.
-  scheduleUpdate: function(delay) {
-    var self = this;
-    var nextLoad = this.config.updateInterval;
-    if (typeof delay !== "undefined" && delay >= 0) {
-      nextLoad = delay;
+  // Override notification handler.
+  notificationReceived: function (notification, payload, sender) {
+    if (notification === "DOM_OBJECTS_CREATED") {
+      this.sendSocketNotification("CONFIG", this.config);
     }
-    setTimeout(function() {
-      self.sendSocketNotification("GET_CHARGER_DATA", self.config);
-    }, nextLoad);
   },
 
-  // Handle notifications from node_helper.
-  socketNotificationReceived: function(notification, payload) {
-    if (notification === "CHARGER_DATA_RESULT") {
-      if (payload.error) {
-        Log.error(`Error getting charger data: ${payload.error}`);
-        return;
-      }
-      Log.info("Received charger data");
-      this.chargerData = payload.chargerData;
-      this.updateDom(1000);
+  // Override socket notification handler.
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "DATA") {
+      this.energyData = payload;
+      this.loaded = true;
+      this.updateDom();
     }
-  }
+  },
+
+  // Helper function to fetch data from the Zaptec API.
+  getData: function () {
+    this.sendSocketNotification("GET_DATA");
+  },
+
+  // Helper function to schedule regular data updates.
+  scheduleUpdate: function () {
+    var self = this;
+    setInterval(function () {
+      self.getData();
+    }, this.config.updateInterval);
+  },
 });
+
