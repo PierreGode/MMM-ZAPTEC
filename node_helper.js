effac2974941404203fd333dbd992d8686bc9a7b
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-04-20 13:59:00 +0200

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,96 +1,42 @@
+const request = require("request");
 const NodeHelper = require("node_helper");
-const axios = require("axios");
 
 module.exports = NodeHelper.create({
-  start: function() {
-    console.log(`Starting helper: ${this.name}`);
-    setInterval(() => {
-      console.log("Retrieving charger data");
-      const options = {
-        method: "GET",
-        url: "https://api.zaptec.com/api/chargers",
-        headers: {
-          "Authorization": "Bearer " + this.config.bearerToken,
-          "accept": "text/plain"
-        }
-      };
-      this.makeRequest(options);
-    }, this.config.updateInterval);
+  // Override start method.
+  start: function () {
+    console.log(`Starting node helper for module [${this.name}]`);
   },
 
-  socketNotificationReceived: function(notification, payload) {
-    console.log("Received socket notification:", notification, "with payload:", payload);
-
-    if (notification === "GET_CHARGER_DATA") {
+  // Override socket notification handler.
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "CONFIG") {
       this.config = payload;
-      console.log("Retrieving charger data");
-      const options = {
-        method: "GET",
-        url: "https://api.zaptec.com/api/chargers",
-        headers: {
-          "Authorization": "Bearer " + payload.bearerToken,
-          "accept": "text/plain"
-        }
-      };
-      this.makeRequest(options);
-    } else if (notification === "GET_CHARGE_HISTORY") {
-      this.config = payload;
-      console.log("Retrieving charge history");
-      const options = {
-        method: "GET",
-        url: "https://api.zaptec.com/api/chargehistory",
-        headers: {
-          "Authorization": "Bearer " + payload.bearerToken,
-          "accept": "text/plain"
-        },
-        params: {
-          charger: payload.charger,
-          fromDate: payload.fromDate,
-          toDate: payload.toDate
-        }
-      };
-      this.getChargeHistory(options);
+      this.getData();
     }
   },
 
-  makeRequest: function(options) {
-    const self = this;
-    axios(options)
-      .then(function(response) {
-        if (response.status === 200) {
-          const chargerData = response.data.Data;
-          console.log("Got charger data:", chargerData);
-          self.sendSocketNotification("CHARGER_DATA_RESULT", { chargerData: chargerData });
-        } else {
-          console.error(`Error getting charger data: ${response.statusText}`);
-          self.sendSocketNotification("CHARGER_DATA_RESULT", { error: response.statusText });
-        }
-      })
-      .catch(function(error) {
-        console.error(`Error getting charger data: ${error}`);
-        self.sendSocketNotification("CHARGER_DATA_RESULT", { error: error.message });
-      });
-  },
+  // Helper function to fetch data from the Zaptec API.
+  getData: function () {
+    var self = this;
+    var options = {
+      url: "https://api.zaptec.com/api/chargehistory",
+      headers: {
+        Authorization: `Bearer ${this.config.token}`,
+      },
+      json: true,
+    };
 
-getChargeHistory: function(options) {
-  const self = this;
-  axios(options)
-    .then(function(response) {
-      if (response.status === 200) {
-        const chargeHistory = response.data.Data;
-        console.log("Got charge history:", chargeHistory);
-        // Get the last session only
-        const lastSession = chargeHistory[chargeHistory.length - 1];
-        const energyData = [lastSession.Energy];
-        self.sendSocketNotification("CHARGE_HISTORY_RESULT", { energyData: energyData });
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var energyData = body.Data.map(function (item) {
+          return item.Energy;
+        });
+        self.sendSocketNotification("DATA", energyData);
       } else {
-        console.error(`Error getting charge history: ${response.statusText}`);
-        self.sendSocketNotification("CHARGE_HISTORY_RESULT", { error: response.statusText });
+        console.log(
+          `Error getting data from Zaptec API: ${error} (${response.statusCode})`
+        );
       }
-    })
-    .catch(function(error) {
-      console.error(`Error getting charge history: ${error}`);
-      self.sendSocketNotification("CHARGE_HISTORY_RESULT", { error: error.message });
     });
-}
+  },
 });
