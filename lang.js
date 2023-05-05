1683e3ead910dc6fe02b9a245f1f9e1095d5a6b4
PierreGode <8579922+PierreGode@users.noreply.github.com>
2023-05-05 07:54:21 +0200

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,128 @@
+{
+  "eng": {
+    "available": "Available",
+    "authorizing": "Authorizing",
+    "charging": "Charging",
+    "finishedCharging": "Finished charging",
+    "charger": "Charger"
+  },
+  "swe": {
+    "available": "Ledigt",
+    "authorizing": "Auktoriserar",
+    "charging": "Laddar",
+    "finishedCharging": "Slutade ladda",
+    "charger": "Laddare"
+  },
+  "dan": {
+    "available": "Tilgængelig",
+    "authorizing": "Autoriserer",
+    "charging": "Oplader",
+    "finishedCharging": "Opladning afsluttet",
+    "charger": "Oplader"
+  },
+  "nor": {
+    "available": "Tilgjengelig",
+    "authorizing": "Autoriserer",
+    "charging": "Lader",
+    "finishedCharging": "Lading fullført",
+    "charger": "Lader"
+  },
+  "isl": {
+    "available": "Laus",
+    "authorizing": "Að heimila",
+    "charging": "Hleður",
+    "finishedCharging": "Hleðsla lokið",
+    "charger": "Hleðslutæki"
+  },
+  "dut": {
+    "available": "Beschikbaar",
+    "authorizing": "Autoriseren",
+    "charging": "Opladen",
+    "finishedCharging": "Opladen voltooid",
+    "charger": "Oplader"
+  },
+  "chi": {
+    "available": "可用",
+    "authorizing": "授权中",
+    "charging": "充电中",
+    "finishedCharging": "充电完成",
+    "charger": "充电器"
+  },
+  "kor": {
+    "available": "사용 가능",
+    "authorizing": "인증 중",
+    "charging": "충전 중",
+    "finishedCharging": "충전 완료",
+    "charger": "충전기"
+  },
+  "pol": {
+    "available": "Dostępny",
+    "authorizing": "Autoryzacja",
+    "charging": "Ładowanie",
+    "finishedCharging": "Zakończono ładowanie",
+    "charger": "Ładowarka"
+  },
+  "fin": {
+    "available": "Saatavilla",
+    "authorizing": "Valtuuttaminen",
+    "charging": "Lataaminen",
+    "finishedCharging": "Lataus valmis",
+    "charger": "Laturi"
+  },
+  "spa": {
+    "available": "Disponible",
+    "authorizing": "Autorizando",
+    "charging": "Cargando",
+    "finishedCharging": "Finalizó la carga",
+    "charger": "Cargador"
+  },
+  "ger": {
+    "available": "Verfügbar",
+    "authorizing": "Autorisierung",
+    "charging": "Laden",
+    "finishedCharging": "Ladevorgang abgeschlossen",
+    "charger": "Ladegerät"
+  },
+  "fre": {
+    "available": "Disponible",
+    "authorizing": "Autorisation",
+    "charging": "En charge",
+    "finishedCharging": "Charge terminée",
+    "charger": "Chargeur"
+  },
+  "ita": {
+    "available": "Disponibile",
+    "authorizing": "Autorizzazione",
+    "charging": "In carica",
+    "finishedCharging": "Carica completata",
+    "charger": "Caricatore"
+  },
+  "por": {
+    "available": "Disponível",
+    "authorizing": "Autorizando",
+    "charging": "Carregando",
+    "finishedCharging": "Carregamento concluído",
+    "charger": "Carregador"
+  },
+  "hin": {
+    "available": "उपलब्ध",
+    "authorizing": "प्राधिकरण",
+    "charging": "चार्ज हो रहा है",
+    "finishedCharging": "चार्जिंग समाप्त",
+    "charger": "चार्जर"
+  },
+  "ara": {
+    "available": "متاح",
+    "authorizing": "تفويض",
+    "charging": "شحن",
+    "finishedCharging": "انتهى الشحن",
+    "charger": "شاحن"
+  },
+  "jpn": {
+    "available": "利用可能",
+    "authorizing": "承認中",
+    "charging": "充電中",
+    "finishedCharging": "充電完了",
+    "charger": "充電器"
+  }
+}
